5c8b6341a4547c330bbc37915dc88b27e576b744
Brady <brady.savarie@hotmail.com>
2023-03-08 18:30:36 -0500

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
