e28f4be463f3f19928b1e313d47587c4485c0a46
Brady <brady.savarie@hotmail.com>
2023-03-08 20:11:05 -0500

    Include task class index properties

--- src/tasks.js
+++ src/tasks.js
@@ -1,9 +1,43 @@
+const tasks = [];
+
 class Task {
-    constructor(title, description, dueDate, priority) {
+    constructor(
+        title,
+        description,
+        dueDate,
+        priority,
+        tasksIndex,
+        projectsIndex
+    ) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
         this.completed = false;
+        this.tasksIndex = tasksIndex;
+        this.projectsIndex = projectsIndex;
     }
 }
+
+function create(
+    title,
+    description,
+    dueDate,
+    priority,
+    tasksIndex,
+    projectsIndex
+) {
+    tasks.push(
+        new Task(
+            title,
+            description,
+            dueDate,
+            priority,
+            tasksIndex,
+            projectsIndex
+        )
+    );
+}
+
+create('something', 'anything', 123, 0, 0, 0);
+console.log(tasks);
