7a85f12d4338f31401ca159707f7f08c94531aee
Brady <brady.savarie@hotmail.com>
2023-03-10 23:44:52 -0500

    Expand pseudocode

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ import './handlers';
 import { createProject, projectsList } from './projects';
 import { createTask } from './tasks';
 
-createProject('title', 'descripton');
-createTask('title', 'description', '01-01-1998', 0, 0, 0);
+createProject(
+    'Default Project',
+    'This project is created during the initial load up'
+);
+createTask(
+    'Default Task',
+    'This task is created during the initial load up',
+    '01-01-1998',
+    0,
+    0,
+    0
+);
 console.log(projectsList);
