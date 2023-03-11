5226faf73a98416316a0417336cc83ceafa1eb7d
Brady <brady.savarie@hotmail.com>
2023-03-10 20:52:29 -0500

    Configure tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
             {
                 test: /\.css$/i,
                 include: path.resolve(__dirname, 'src'),
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
             },
         ],
     },
