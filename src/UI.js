b35350e282711e1282da36b05602cd2537abfd08
Brady <brady.savarie@hotmail.com>
2023-03-16 00:24:07 -0400

    Continue styling project cards

--- src/UI.js
+++ src/UI.js
@@ -21,31 +21,40 @@ function renderNewProjectCard(projectsIndex) {
     const projectsContainer = document.getElementById('projectsContainer');
     const newProjectCard = document.createElement('div');
 
-    /* newProjectCard.innerHTML = `<div
-                            data-element="openReadProjectModalBtn"
-                            class="flex flex-col cursor-pointer rounded-lg bg-red-600 text-white w-[65%] py-12 font-Lato font-bold shadow-md"
-                        >
-                            <button data-projectsIndex=${projectsIndex} class="text-2xl">
-                                My Tasks
-                            </button>
-                            <!-- <button data-element="openDeleteProjectModalBtn">
-                                Delete Project
-                            </button> -->
-                        </div>`;
-    projectsContainer.appendChild(newProjectCard); */
     projectsContainer.insertAdjacentHTML(
         'beforeend',
         `<div
-                        class="h-full min-w-[60%] flex grow-0 shrink-0 basis-auto snap-center"
+                        class="h-full min-w-[60%] flex grow shrink-0 basis-auto snap-center shadow-md"
                     >
                         <div
                             data-element="openReadProjectModalBtn"
-                            class="flex flex-col justify-start items-start cursor-pointer rounded-xl bg-gradient-to-b from-orange-400 to-orange-500 text-white w-full font-Lato font-bold pl-10 pt-14"
+                            class="flex flex-col justify-start items-start cursor-pointer rounded-xl bg-gradient-to-b from-orange-400 to-orange-500 text-white w-full font-Lato font-bold pl-10 pt-14 shadow-md"
                         >
-                            <button data-projectsIndex="0" class="text-xl mb-5">
+                            <button
+                                data-projectsIndex="0"
+                                class="text-2xl mb-5"
+                            >
                                 My Tasks
                             </button>
                             <div class="h-[1.25px] w-full bg-white"></div>
+                            <div class="h-full flex flex-col mt-5 gap-3">
+                                <div class="flex items-center gap-4 text-xl">
+                                    <i class="fa-regular fa-square"></i>
+                                    <p class="font-normal">Download App</p>
+                                </div>
+                                <div class="flex items-center gap-4 text-xl">
+                                    <i class="fa-regular fa-square"></i>
+                                    <p class="font-normal">Download App</p>
+                                </div>
+                                <div class="flex items-center gap-4 text-xl">
+                                    <i class="fa-regular fa-square"></i>
+                                    <p class="font-normal">Download App</p>
+                                </div>
+                                <div class="flex items-center gap-4 text-xl">
+                                    <i class="fa-regular fa-square"></i>
+                                    <p class="font-normal">Download App</p>
+                                </div>
+                            </div>
                             <!-- <button data-element="openDeleteProjectModalBtn">
                                 Delete Project
                             </button> -->
