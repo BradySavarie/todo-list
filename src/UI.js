0d656f2b46f88f8d20a577e3d9d2c2cbd1bd1b9f
Brady <brady.savarie@hotmail.com>
2023-03-20 23:51:55 -0400

    Render tasks to project cards

--- src/UI.js
+++ src/UI.js
@@ -28,34 +28,30 @@ function renderProjectCards() {
         projectsScroller.insertAdjacentHTML(
             'afterbegin',
             `<div
-                    data-projectKey=${project.id} class="h-full min-w-[80%] flex grow shrink-0 basis-auto snap-center shadow-md"
+                    data-projectKey=${project.id} class="h-full min-w-[80%] overflow-y-scroll flex grow shrink-0 basis-auto snap-center shadow-md"
                     >
             <div
                 class="flex flex-col justify-start items-start cursor-pointer rounded-xl bg-gradient-to-b from-orange-400 to-orange-500 text-white w-full font-Lato font-bold pl-10 pt-14 shadow-md"
             >
                 <p class="text-2xl mb-5">${project.title}</p>
                 <div class="h-[1.25px] w-full bg-white"></div>
-                <div class="h-full flex flex-col mt-5 gap-3">
-                    <div class="flex items-center gap-4 text-xl">
-                        <i class="fa-regular fa-square"></i>
-                        <p class="font-normal">Buy Groceries</p>
-                    </div>
-                    <div class="flex items-center gap-4 text-xl">
-                        <i class="fa-regular fa-square"></i>
-                        <p class="font-normal">Pay Bills</p>
-                    </div>
-                    <div class="flex items-center gap-4 text-xl">
-                        <i class="fa-regular fa-square"></i>
-                        <p class="font-normal">Cook Dinner</p>
-                    </div>
-                    <div class="flex items-center gap-4 text-xl">
-                        <i class="fa-regular fa-square"></i>
-                        <p class="font-normal">Feed Cats</p>
-                    </div>
+                <div  id='projectCardTasksContainer' class="overflow-y-scroll h-full w-full flex flex-col mt-5 gap-3">
                 </div>
             </div>
         </div>`
         );
+        project.tasks.forEach((task) => {
+            const projectCardTasksContainer = document.getElementById(
+                'projectCardTasksContainer'
+            );
+            projectCardTasksContainer.insertAdjacentHTML(
+                'beforeend',
+                `<div class="flex items-center gap-4 text-xl">
+                        <i class="fa-regular fa-square"></i>
+                        <p class="font-normal">${task.title}</p>
+                </div>`
+            );
+        });
     });
 }
 
